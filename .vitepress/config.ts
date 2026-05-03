1ec60b00def00ab38e41c6b026642a467cdf8e7c
imsunxinhao <imsunxinhao@163.com>
2026-05-03 20:02:45 +0800

    Create VitePress Homepage Config

---
+++ .vitepress/config.ts
@@ -0,0 +1,15 @@
+import { defineConfigWithTheme } from 'vitepress'
+export default {
+  lang: 'zh-CN',
+  title: '众生之门社区开发组',
+  description: '众生之门官方非营利社区组织，致力于创造更有趣的社区体验，为成员提供优质服务。',
+  sidebar: [
+    {
+      text: '导航',
+      items: [
+        { text: '关于我们', link: '/about-us' },
+        { text: '加入我们', link: '/join-us' },
+      ]
+    }
+  ]
+}
